474aa5cdbba6960170c0696db072a7e12aed969b
Gabriel Schenkel <gabriel95gsa@outlook.com>
2023-08-24 19:06:04 -0300

    changing test conf to specific .env.testing

--- tests/Feature/ExpenseControllerTest.php
+++ tests/Feature/ExpenseControllerTest.php
@@ -4,23 +4,80 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class ExpenseControllerTest extends TestCase
 {
-
-    protected function test_get_all_resources()
+    public function test_get_all_resources_return_200_status(): void
     {
-        // TODO: Implement test_get_all_resources() method.
+        $response = $this->getJson($this->apiUri, [
+            'Authorization' => $this->authorizationToken
+        ])->assertStatus(Response::HTTP_OK)->json('data');
     }
 
-    protected function test_create_resource_return_201_status()
+    public function test_create_resource_return_201_status(): void
     {
         // TODO: Implement test_create_resource_return_201_status() method.
     }
 
-    protected function test_create_resource_return_422_status_on_incorrect_request_field()
+    public function test_create_resource_return_422_status_on_incorrect_request_field(): void
     {
         // TODO: Implement test_create_resource_return_422_status_on_incorrect_request_field() method.
+    }
+
+    public function test_get_resource_by_id_return_200_status(): void
+    {
+        // TODO: Implement test_get_resource_by_id_return_200_status() method.
+    }
+
+    public function test_get_resource_by_id_return_404_status_on_nonexistent_resource_id(): void
+    {
+        // TODO: Implement test_get_resource_by_id_return_404_status_on_nonexistent_resource_id() method.
+    }
+
+    public function test_get_resource_by_id_return_403_status_on_access_forbidden(): void
+    {
+        // TODO: Implement test_get_resource_by_id_return_403_status_on_access_forbidden() method.
+    }
+
+    public function test_update_resource_return_200_status(): void
+    {
+        // TODO: Implement test_update_resource_return_200_status() method.
+    }
+
+    public function test_update_resource_return_422_status_on_incorrect_request_field(): void
+    {
+        // TODO: Implement test_update_resource_return_422_status_on_incorrect_request_field() method.
+    }
+
+    public function test_update_resource_return_404_status_on_nonexistent_resource_id(): void
+    {
+        // TODO: Implement test_update_resource_return_404_status_on_nonexistent_resource_id() method.
+    }
+
+    public function test_update_resource_return_403_status_on_access_forbidden(): void
+    {
+        // TODO: Implement test_update_resource_return_403_status_on_access_forbidden() method.
+    }
+
+    public function test_delete_resource_return_200_status(): void
+    {
+        // TODO: Implement test_delete_resource_return_200_status() method.
+    }
+
+    public function test_delete_resource_return_422_status_on_incorrect_request_field(): void
+    {
+        // TODO: Implement test_delete_resource_return_422_status_on_incorrect_request_field() method.
+    }
+
+    public function test_delete_resource_return_404_status_on_nonexistent_resource_id(): void
+    {
+        // TODO: Implement test_delete_resource_return_404_status_on_nonexistent_resource_id() method.
+    }
+
+    public function test_delete_resource_return_403_status_on_access_forbidden(): void
+    {
+        // TODO: Implement test_delete_resource_return_403_status_on_access_forbidden() method.
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,15 +3,41 @@
 namespace Tests;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Hash;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
+    protected string $apiUri;
+
+    protected string $authorizationToken;
+
+    protected User $user;
+
     protected function setUp(): void
     {
         parent::setUp();
+
+//        $this->seed();
+
+        $this->apiUri = 'api/app/';
+
+//        $this->user = $this->createUser([
+//            'name' => 'Test User 1',
+//            'email' => 'testuser1@testing.com',
+//            'password' => Hash::make('1234'),
+//        ]);
+//
+//        $this->user->email_verified_at = Carbon::now();
+//        $this->user->save();
+
+        $authorizationToken = $this->postJson('api/auth/login', [
+            'email' => $this->user->email,
+            'password' => '1234',
+        ])->json('access_token');
     }
 
     protected function createUser(array $attributes): User
@@ -19,31 +45,35 @@ abstract class TestCase extends BaseTestCase
         return User::factory()->create($attributes);
     }
 
-    protected abstract function test_get_all_resources(): void;
+    /**
+     * Abstract method which should be implemented in the feature test classes
+     * For testing all their api methods
+     */
+    public abstract function test_get_all_resources_return_200_status(): void;
 
-    protected abstract function test_create_resource_return_201_status(): void;
+    public abstract function test_create_resource_return_201_status(): void;
 
-    protected abstract function test_create_resource_return_422_status_on_incorrect_request_field(): void;
+    public abstract function test_create_resource_return_422_status_on_incorrect_request_field(): void;
 
-    protected abstract function test_get_resource_by_id_return_200_status(): void;
+    public abstract function test_get_resource_by_id_return_200_status(): void;
 
-    protected abstract function test_get_resource_by_id_return_404_status_on_nonexistent_resource_id(): void;
+    public abstract function test_get_resource_by_id_return_404_status_on_nonexistent_resource_id(): void;
 
-    protected abstract function test_get_resource_by_id_return_403_status_on_access_forbidden(): void;
+    public abstract function test_get_resource_by_id_return_403_status_on_access_forbidden(): void;
 
-    protected abstract function test_update_resource_return_200_status(): void;
+    public abstract function test_update_resource_return_200_status(): void;
 
-    protected abstract function test_update_resource_return_422_status_on_incorrect_request_field(): void;
+    public abstract function test_update_resource_return_422_status_on_incorrect_request_field(): void;
 
-    protected abstract function test_update_resource_return_404_status_on_nonexistent_resource_id(): void;
+    public abstract function test_update_resource_return_404_status_on_nonexistent_resource_id(): void;
 
-    protected abstract function test_update_resource_return_403_status_on_access_forbidden(): void;
+    public abstract function test_update_resource_return_403_status_on_access_forbidden(): void;
 
-    protected abstract function test_delete_resource_return_200_status(): void;
+    public abstract function test_delete_resource_return_200_status(): void;
 
-    protected abstract function test_delete_resource_return_422_status_on_incorrect_request_field(): void;
+    public abstract function test_delete_resource_return_422_status_on_incorrect_request_field(): void;
 
-    protected abstract function test_delete_resource_return_404_status_on_nonexistent_resource_id(): void;
+    public abstract function test_delete_resource_return_404_status_on_nonexistent_resource_id(): void;
 
-    protected abstract function test_delete_resource_return_403_status_on_access_forbidden(): void;
+    public abstract function test_delete_resource_return_403_status_on_access_forbidden(): void;
 }
